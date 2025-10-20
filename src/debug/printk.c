91f53a3ea16017dccbce270d7c4ee8e99668da38
rbardet <rbardet@rbardet.myguest.virtualbox.org>
2025-10-20 06:58:26 +0200

    Add: printk

---
+++ src/debug/printk.c
@@ -0,0 +1,112 @@
+#include "printk.h"
+
+uint16 tty_color = COLOR_WHITE << 8;
+uint16 *vga = (uint16 *)VRAM;
+uint16 column = 0;
+uint16 line = 0;
+
+void set_print_color(unsigned int color) {
+	tty_color = color << 8;
+}
+
+void reset() {
+	line = 0;
+	column = 0;
+	for (uint16 y = 0; y < HEIGHT; y++) {
+		for (uint16 x = 0; x < WIDTH; x++) {
+			vga[y * WIDTH + x] = ' ' | tty_color;
+		}
+	}
+	
+}
+
+static void scroll() {
+	for (uint16 y = 0; y < HEIGHT; y++) {
+		for (uint16 x = 0; x < WIDTH; x++) {
+			vga[(y - 1) * WIDTH + x] = vga[y * WIDTH + x];
+		}
+	}
+	
+	for (uint16 x = 0; x < HEIGHT; x++) {
+		vga[(HEIGHT - 1) * WIDTH + x] = ' ' | tty_color;
+	}
+}
+
+static void newLine() {
+	if (line < HEIGHT - 1) {
+		line++;
+		column = 0;
+	} else {
+		scroll();
+		column = 0;
+	}
+}
+
+void print_terminal(const char text) {
+	switch (text) {
+		case '\n':
+			newLine();
+			break;
+		case '\r':
+			column = 0;
+			break;
+		case '\t':
+			if (column == WIDTH) {
+				newLine();
+			}
+			uint16 tabLen = 4 - (column % 4);
+			while (tabLen != 0) {
+				vga[line * WIDTH + (column++)] = ' ' | tty_color;
+				tabLen--;
+			}
+			break;
+		default:
+			if (column == WIDTH) {
+				newLine();
+			}
+			vga[line * WIDTH + (column++)] = text | tty_color;
+			break ;
+	}
+}
+
+static int putstrk(const char *str) {
+	while (*str) {
+		print_terminal(*str);
+		str++;
+	}
+	return (strlen(str));
+}
+
+static int printk_format(va_list list, const char format) {
+	int len = 0;
+
+	switch (format) {
+		case 'c':
+			print_terminal(va_arg(list, char));
+			return (1);
+		case 's':
+			const char *str = va_arg(list, char *);
+			return (putstrk(str));
+	}
+	return (len);
+}
+
+int printk(const char *format, ...) {
+	va_list arg;
+	int len = 0;
+
+	va_start(arg, format);
+	while (*format) {
+		if (*format == '%' && *(format + 1)) {
+			len += printk_format(arg, *format);
+			format += 2;
+		} else {
+			print_terminal(*format);
+			format++;
+			len++;
+		}
+	}
+	va_end(arg);
+
+	return (len);
+}
---
+++ src/debug/printk.h
@@ -0,0 +1,47 @@
+#ifndef PRINTK_H
+#define PRINTK_H
+
+#define VRAM 0xb8000
+
+#include "../lib/string/string.h"
+#include "../lib/type.h"
+#include "stdarg.h"
+
+#define WIDTH 80
+#define HEIGHT 25
+
+typedef enum {
+	COLOR_BLACK,
+	COLOR_BLUE,
+	COLOR_GREEN,
+	COLOR_CYAN,
+	COLOR_RED,
+	COLOR_MAGENTA,
+	COLOR_BROWN,
+	COLOR_LIGHT_GRAY,
+	COLOR_DARK_GRAY,
+	COLOR_LIGHT_BLUE,
+	COLOR_LIGHT_GREEN,
+	COLOR_LIGHT_CYAN,
+	COLOR_LIGHT_RED,
+	COLOR_LIGHT_MAGENTA,
+	COLOR_YELLOW,
+	COLOR_WHITE
+} color;
+
+typedef enum {
+	KERN_EMERG,
+	KERN_ALERT,
+	KERN_CRIT,
+	KERN_ERR,
+	KERN_WARNING,
+	KERN_NOTICE,
+	KERN_INFO,
+	KERN_DEBUG,
+} kinfo;
+
+void set_print_color(unsigned int color);
+int printk(const char *format, ...);
+void reset();
+
+#endif

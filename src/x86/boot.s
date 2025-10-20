8d031f5c26a02004f3b42f1aa399ed4007cd834c
rbardet <rbardet@rbardet.myguest.virtualbox.org>
2025-10-21 00:47:37 +0200

    Add: keyboard interrupt

--- src/x86/boot.s
+++ src/x86/boot.s
@@ -5,9 +5,28 @@ section .text
 	DD -(0x1BADB002 + 0x00000000)
 
 global start
-extern kmain
+global inb
+global outb
 
+extern kmain
+extern keyboard_interrupt
 start:
 	CALL kmain
-HaltKernel:
-	JMP HaltKernel
+
+keyboard_handler:
+	PUSHAD
+	CALL keyboard_interrupt
+	POPAD
+	IRETD
+
+inb:
+	MOV edx, [esp + 4]
+	IN al, dx
+	RET
+
+outb:
+	MOV edx, [esp + 4]
+	MOV ebx, [esp + 8]
+	OUT dx, al
+	RET
+

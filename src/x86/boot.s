199441d6efa648655e961e3410faf3246430cceb
rbardet <rbardet@rbardet.myguest.virtualbox.org>
2025-10-22 02:17:45 +0200

    rm: useless func

--- src/x86/boot.s
+++ src/x86/boot.s
@@ -7,37 +7,17 @@ section .text
 global start
 global inb
 global outb
-global keyboard_handler
-global load_idt
-global enable_interrupt
 extern kmain
-extern keyboard_interrupt
+
 start:
 	CALL kmain
-HaltKernel:
-	JMP HaltKernel
-keyboard_handler:
-	PUSHA
-	CALL keyboard_interrupt
-	MOV al, 0x20
-	OUT 0x20, al
-	POPA
-	IRETD
-
-enable_interrupt:
-	STI
-	RET
 inb:
 	MOV edx, [esp + 4]
 	IN al, dx
 	RET
 
 outb:
-	MOV al, [esp + 4]
+	MOV eax, [esp + 4]
 	MOV edx, [esp + 8]
 	OUT dx, al
 	RET
-load_idt:
-	mov edx, [esp + 4]
-	lidt [edx]
-	ret

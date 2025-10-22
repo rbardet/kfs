d677903240cd858a70b4a991b246e7b1c4870db9
rbardet <rbardet@rbardet.myguest.virtualbox.org>
2025-10-22 02:19:38 +0200

    Fix: working prompt and cursor

--- src/kernel.c
+++ src/kernel.c
@@ -1,19 +1,18 @@
 #include "graphic/printk.h"
 #include "graphic/switch_screen.h"
-#include "interrupt/idt.h"
 #include "hardware/keyboard/keyboard.h"
+#include "shell/shell.h"
 #include "lib/lib.h"
 
 void kmain() {
+	uint8 buffer[BUFFER_SIZE];
 	terminal_initialize();
-	printk("Starting...\n");
-	
-	init_idt();
-	printk("IDT loaded\n");
-	
-	init_kb();
-	printk("KB initialized\n");
-		
-	// enable_interrupt();
-	// printk("Interrupts enabled!\n");
+	set_print_color(COLOR_LIGHT_MAGENTA);
+	printk("42 - kfs1\n");
+
+	while (1) {
+		printk("> ");
+		memset(buffer, 0, sizeof(buffer));
+		shell(buffer, sizeof(buffer));
+	}
 }

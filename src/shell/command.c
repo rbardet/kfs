6bd1d38becfd4a35c1bd200d236a5f39d70cf4d7
rbardet <robin.bardet.tomczak@gmail.com>
2025-10-31 14:18:19 +0100

    Feat: separate function for command

--- src/shell/command.c
+++ src/shell/command.c
@@ -1,63 +1,93 @@
 #include "command.h"
 
+static void switch_color(uint8 c) {
+	if (c >= '1' && c <= '9') {
+		set_print_color(c - '0');
+		return;
+	} else {
+		printk("Invalid color code. Use a number between 1 and 9.\n");
+		return;
+	}
+}
+
+static void switch_terminal(uint8 t) {
+	if (t >= 0 && t <= 9) {
+		switch_terminal(t);
+		return;
+	} else {
+		printk("Invalid terminal number. Use a number between 0 and 9.\n");
+		return;
+	}
+}
+
+static void reboot() {
+	printk("Rebooting system...\n");
+	uint8 good = 0x02;
+	while (good & 0x02) 
+		good = inb(0x64);
+	outb(0xFE, 0x64);
+	return;
+}
+
+static void shutdown() {
+	printk("Shutting down system...\n");
+	outw(0x2000, 0x604);
+	return;
+}
+
+static void help() {
+	printk("Available commands:\n");
+	printk(" - clear: Clear the terminal screen\n");
+	printk(" - help: Show this help message\n");
+	printk(" - color=<1-9>: Change text color\n");
+	printk(" - terminal=<0-9>: Switch to terminal number\n");
+	return;
+}
+
 void execute_command(char *command) {
-    if (strncmp(command, "clear", 5) == 0) {
-        reset();
-        return;
-    }
+	if (strcmp(command, "clear") == 0) {
+		reset();
+		return;
+	}
 
-    if (strncmp(command, "color=", 6) == 0) {
-        uint8 c = command[6];
-        if (c >= '1' && c <= '9') {
-            set_print_color(c - '0');
-            return;
-        } else {
-            printk("Invalid color code. Use a number between 1 and 9.\n");
-            return;
-        }
-    }
+	if (strncmp(command, "color=", 6) == 0) {
+		uint8 c = command[6];
+		switch_color(c);
+		return ;
+	}
 
-    if (strncmp(command, "terminal=", 9) == 0) {
-        uint8 t = command[9] - '0';
-        if (t >= 0 && t <= 9) {
-            switch_terminal(t);
-            return;
-        } else {
-            printk("Invalid terminal number. Use a number between 0 and 9.\n");
-            return;
-        }
-    }
+	if (strncmp(command, "terminal=", 9) == 0) {
+		uint8 t = command[9] - '0';
+		switch_terminal(t);
+		return ;
+	}
 
-    if (strncmp(command, "reboot", 6) == 0) {
-        printk("Rebooting system...\n");
-        uint8 good = 0x02;
-        while (good & 0x02)
-            good = inb(0x64);
-        outb(0xFE, 0x64);
-        return;
-    }
+	if (strcmp(command, "reboot") == 0) {
+		reboot();
+		return ;
+	}
 
-    if (strncmp(command, "shutdown", 8) == 0) {
-        printk("Shutting down system...\n");
-        outw(0x2000, 0x604);
-        return;
-    }
+	if (strcmp(command, "shutdown") == 0) {
+		shutdown();
+		return ;
+	}
 
-    if (strncmp(command, "halt", 4) == 0) {
-        printk("Halting CPU...\n");
-        // TODO: Implement halt functionality
-        return;
-    }
+	if (strcmp(command, "halt") == 0) {
+		printk("Halting CPU...\n");
+		// TODO: Implement halt functionality
+		return;
+	}
 
-    if (strncmp(command, "help", 4) == 0) {
-        printk("Available commands:\n");
-        printk(" - clear: Clear the terminal screen\n");
-        printk(" - help: Show this help message\n");
-        printk(" - color=<1-9>: Change text color\n");
-        printk(" - terminal=<0-9>: Switch to terminal number\n");
-        return;
-    }
+	if (strcmp(command, "help") == 0) {
+		help();
+		return ;
+	}
 
-    printk("Unknown command: %s\n", command);
-    return;
+	if (strcmp(command, "stack") == 0) {
+		printk("%p\n", get_stack_top());
+		return;
+	}
+
+	printk("Unknown command: %s\n", command);
+	return;
 }

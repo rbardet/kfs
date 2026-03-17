c758e4b8e7dbcef216f118397f5909dbaa236935
rbardet <robin.bardet.tomczak@gmail.com>
2026-03-17 13:23:58 +0100

    Feat: add define for cursor valueg

--- src/shell/cursor.c
+++ src/shell/cursor.c
@@ -2,13 +2,19 @@
 
 // https://wiki.osdev.org/Text_Mode_Cursor
 
+#define CURSOR_REGISTER_SELECTOR 0x3D4
+#define CURSOR_VALUE_PORT 0x3D5
+
+#define CURSOR_LOW 0x0F
+#define CURSOR_HIGH 0x0E
+
 void update_cursor() {
 	u16 pos = line * WIDTH + column;
 
-	outb(0x3D4, 0x0F);
-	outb(0x3D5, (u8) (pos & 0xFF));
-	outb(0x3D4, 0x0E);
-	outb(0x3D5, (u8) ((pos >> 8) & 0xFF));
+	outb(CURSOR_REGISTER_SELECTOR, CURSOR_LOW);
+	outb(CURSOR_VALUE_PORT, (u8) (pos & 0xFF));
+	outb(CURSOR_REGISTER_SELECTOR, CURSOR_HIGH);
+	outb(CURSOR_VALUE_PORT, (u8) ((pos >> 8) & 0xFF));
 }
 
 void move_cursor(arrow dir) {
